884f4c70d04d3c08e73b9fad145bad258db918fc
hangx-ma <m-contour@qq.com>
2024-02-04 23:08:54 +0800

    fix: actually fix vsnip syntax error

--- nvim/lua/lsp/cmp.lua
+++ nvim/lua/lsp/cmp.lua
@@ -3,8 +3,8 @@ cmp.setup({
     snippet = {
         -- REQUIRED - you must specify a snippet engine
         expand = function(args)
-            -- vim.fn["vsnip#anymous"](args.body) -- For `vsnip` users, it fails
-            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+            vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users
+            -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
         end,
     },
 
