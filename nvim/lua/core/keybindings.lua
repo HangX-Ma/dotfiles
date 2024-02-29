d5501b647bc78dad719ae0c28ac423400f68bb4e
HangX-Ma <m-contour@qq.com>
2024-02-29 17:36:08 +0800

    feat: update nvim config
    Signed-off-by: HangX-Ma <m-contour@qq.com>

--- nvim/lua/core/keybindings.lua
+++ nvim/lua/core/keybindings.lua
@@ -3,52 +3,52 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 local keymap = vim.keymap
-local opt = { noremap = true, silent = true }
+local opts = { noremap = true, silent = true }
 
 -- ---------- Usage Description ----------
 -- <C-?> means 'ctrl + ?'
 -- <A-?> means 'alt + ?'
 -- ---------------------------------------
 
-keymap.set("i", "<C-[>", "<ESC>", opt)
-keymap.set("n", "<leader>q", ":q!<CR>", opt)
-keymap.set("n", "<leader>Q", ":qa!<CR>", opt)
-keymap.set("n", "<leader>x", ":x<CR>", opt)
-keymap.set("n", "<leader>X", ":xa<CR>", opt)
+keymap.set("i", "<C-[>", "<ESC>", opts)
+keymap.set("n", "<leader>q", ":q!<CR>", opts)
+keymap.set("n", "<leader>Q", ":qa!<CR>", opts)
+keymap.set("n", "<leader>x", ":x<CR>", opts)
+keymap.set("n", "<leader>X", ":xa<CR>", opts)
 
 -- visual line
 -- no highlight
-keymap.set("n", "<leader>nh", ":nohl<CR>", opt)
+keymap.set("n", "<leader>nh", ":nohl<CR>", opts)
 
 -- split windows vertically or horizontally
-keymap.set("n", "<leader>sv", ":vsp<CR>", opt)
-keymap.set("n", "<leader>sh", ":sp<CR>", opt)
+keymap.set("n", "<leader>sv", ":vsp<CR>", opts)
+keymap.set("n", "<leader>sh", ":sp<CR>", opts)
 
 -- close current window <C-w>c
 -- close other window <C-w>o
 
 -- indent code in 'Normal Mode'
-keymap.set("v", "<", "<gv", opt)
-keymap.set("v", ">", ">gv", opt)
+keymap.set("v", "<", "<gv", opts)
+keymap.set("v", ">", ">gv", opts)
 
 -- move code up or down in 'Normal Mode'
-keymap.set("v", "J", ":move '>+1<CR>gv-gv", opt)
-keymap.set("v", "K", ":move '<-2<CR>gv-gv", opt)
+keymap.set("v", "J", ":move '>+1<CR>gv-gv", opts)
+keymap.set("v", "K", ":move '<-2<CR>gv-gv", opts)
 
 -- control window left/right size
-keymap.set("n", "<C-Left>", ":vertical resize +2<CR>", opt)
-keymap.set("n", "<C-Right>", ":vertical resize -2<CR>", opt)
+keymap.set("n", "<C-Left>", ":vertical resize +2<CR>", opts)
+keymap.set("n", "<C-Right>", ":vertical resize -2<CR>", opts)
 
 -- control window up/down size
-keymap.set("n", "<C-Down>", ":resize +2<CR>", opt)
-keymap.set("n", "<C-Up>", ":resize -2<CR>", opt)
+keymap.set("n", "<C-Down>", ":resize +2<CR>", opts)
+keymap.set("n", "<C-Up>", ":resize -2<CR>", opts)
 
 -- equivalent scale: <C-w>=
 
 -- ------------- extensions -------------
 local pluginKeys = {}
 -- toogle explorer
-keymap.set("n", "<C-N>", ":NvimTreeToggle<CR>", opt)
+keymap.set("n", "<C-N>", ":NvimTreeToggle<CR>", opts)
 
 -- <leader>tt float
 -- <leader>tv vertical
