8e296f794595d8c5a913791df5b914ddc79dc691
HangX-Ma <m-contour@qq.com>
2024-04-26 17:35:06 +0800

    feat: enhance lsp experiences and add support cscope

---
+++ nvim/lua/plugins/lsp/lsp_signature.lua
@@ -0,0 +1,22 @@
+return {
+	"ray-x/lsp_signature.nvim",
+	event = "VeryLazy",
+	opts = {
+		bind = true, -- This is mandatory, otherwise border config won't get registered.
+		handler_opts = {
+			border = "rounded",
+		},
+	},
+	config = function(_, opts)
+		local lspsign = require("lsp_signature")
+		lspsign.setup(opts)
+
+		vim.keymap.set({ "n" }, "<leader>kw", function()
+			lspsign.toggle_float_win()
+		end, { silent = true, noremap = true, desc = "toggle signature" })
+
+		vim.keymap.set({ "n" }, "<leader>kh", function()
+			vim.lsp.buf.signature_help()
+		end, { silent = true, noremap = true, desc = "toggle signature" })
+	end,
+}
