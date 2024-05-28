f90115335e718eaba4bff321e5eeceee21cae333
hangx-ma <m-contour@qq.com>
2024-05-28 22:50:25 +0800

    feat: add rust tools

--- nvim/lua/plugins/lsp/nvim-lint.lua
+++ nvim/lua/plugins/lsp/nvim-lint.lua
@@ -42,9 +42,6 @@ return {
 					lint.try_lint()
 				end,
 			})
-			vim.keymap.set("n", "<leader>l", function()
-				lint.try_lint()
-			end, { desc = "Trigger linting for current file" })
 		end,
 	},
 	-- mason-nvim-lint
