c22753235100821fe0feb20e3aba2e96fb53f410
HangX-Ma <m-contour@qq.com>
2024-09-13 10:25:35 +0800

    feat: update startup.nvim config
    Signed-off-by: HangX-Ma <m-contour@qq.com>

--- nvim/lua/plugin/manager/startup.lua
+++ nvim/lua/plugin/manager/startup.lua
@@ -1,10 +1,11 @@
 return {
 	{
 		"startup-nvim/startup.nvim",
+		lazy = false,
 		dependencies = {
-			{ "nvim-telescope/telescope.nvim", lazy = true },
+			"nvim-telescope/telescope.nvim",
 			"nvim-lua/plenary.nvim",
-			"nvim-lua/popup.nvim",
+			"nvim-telescope/telescope-file-browser.nvim",
 		},
 		config = function()
 			require("startup").setup({ theme = "dashboard" })
