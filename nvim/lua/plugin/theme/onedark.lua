bd1c48b7b85e50e369d1d3addaa9802f614e1a37
HangX-Ma <m-contour@qq.com>
2024-10-09 09:57:33 +0800

    feat: add new themes
    Signed-off-by: HangX-Ma <m-contour@qq.com>

--- nvim/lua/plugin/theme/onedark.lua
+++ nvim/lua/plugin/theme/onedark.lua
@@ -1,12 +1,11 @@
 -- atom one dark colortheme
 return {
-    {
-        'navarasu/onedark.nvim',
-        lazy = false, -- make sure we load this during startup if it is your main colorscheme
-        priority = 1000, -- make sure to load this before all the other start plugins
-        config = function()
-          -- load the colorscheme here
-          vim.cmd([[colorscheme onedark]])
-        end,
-    }
+	"navarasu/onedark.nvim",
+	enabled = false,
+	lazy = false, -- make sure we load this during startup if it is your main colorscheme
+	priority = 1000, -- make sure to load this before all the other start plugins
+	config = function()
+		-- load the colorscheme here
+		vim.cmd([[colorscheme onedark]])
+	end,
 }
