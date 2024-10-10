321bd0d0f669bb1676961f8cb1868dc4af40c2f3
HangX-Ma <m-contour@qq.com>
2024-10-10 14:58:25 +0800

    feat: update theme
    Signed-off-by: HangX-Ma <m-contour@qq.com>

--- nvim/lua/plugin/theme/catppuccin.lua
+++ nvim/lua/plugin/theme/catppuccin.lua
@@ -1,12 +1,12 @@
 return {
 	"catppuccin/nvim",
-	enabled = false,
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+	enabled = true,
+	lazy = false, -- make sure we load this during startup if it is your main colorscheme
 	name = "catppuccin",
 	priority = 1000,
 	config = function()
 		-- load the colorscheme here
-        -- catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
+		-- catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
 		vim.cmd([[colorscheme catppuccin-frappe]])
 	end,
 }
