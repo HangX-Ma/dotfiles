c88f355b2e6cc5c8d8307e222666d74693698416
HangX-Ma <m-contour@qq.com>
2025-03-12 20:17:31 +0800

    feat: update loader to optimize start up speed
    Signed-off-by: HangX-Ma <m-contour@qq.com>

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,9 +5,19 @@ vim.api.nvim_create_autocmd("User", {
 	callback = function()
 		require("core.autocmds")
 		require("core.keybindings")
-        require("syntax.c")
-        require("syntax.cpp")
+	end,
+})
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "c", "cpp" },
+	callback = function()
+		require("syntax.c")
+		require("syntax.cpp")
 	end,
 })
 
-require("core.handlers").setup()
+vim.api.nvim_create_autocmd("User", {
+	pattern = "ReadBufPre",
+	callback = function()
+		require("core.handlers").setup()
+	end,
+})
