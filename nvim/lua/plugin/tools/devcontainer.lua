b63fb3e2e758e99578c5d287989f0f4ea518842f
HangX-Ma <m-contour@qq.com>
2025-04-14 21:40:52 +0800

    feat: update neovim configs
    Signed-off-by: HangX-Ma <m-contour@qq.com>

--- nvim/lua/plugin/tools/devcontainer.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"esensar/nvim-dev-container",
-	enabled = false,
-	dependencies = "nvim-treesitter/nvim-treesitter",
-	event = "VeryLazy",
-	config = function()
-		require("devcontainer").setup({})
-	end,
-}
