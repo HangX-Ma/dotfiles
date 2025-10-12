ff3865473e1c31d13ae98c1dfdc8ed7381eeaba3
HangX-Ma <m-contour@qq.com>
2025-10-12 11:34:56 +0800

    feat: Update nvim-config compatibility
    Signed-off-by: HangX-Ma <m-contour@qq.com>

--- nvim/lua/plugin/lsp/server/lua.lua
+++ nvim/lua/plugin/lsp/server/lua.lua
@@ -8,8 +8,7 @@ end
 function server.setup()
 	-- lazydev.nvim configuration
 	-- https://www.reddit.com/r/neovim/comments/1d5ub7d/lazydevnvim_much_faster_luals_setup_for_neovim/
-	local lspconfig = require("lspconfig")
-	lspconfig.lua_ls.setup({
+	local opts = {
 		settings = {
 			Lua = {
 				hint = {
@@ -17,53 +16,16 @@ function server.setup()
 				},
 			},
 		},
-	})
+	}
 
-	-- no need for lazydev.nvim
-	--[[ local runtime_path = vim.split(package.path, ";")
-	table.insert(runtime_path, "lua/?.lua")
-	table.insert(runtime_path, "lua/?/init.lua")
-	local common = require("plugin.lsp.server.common")
-	local custom_attach = require("core.handlers").on_attach
-	local lspconfig = require("lspconfig")
-	lspconfig.lua_ls.setup({
-        on_attach = custom_attach,
-		flags = common.lspflags,
-		capabilities = common.capabilities,
-        before_init = require("neodev.nvim").before_init,
-		on_init = function(client)
-			local path = client.workspace_folders[1].name
-			if not vim.loop.fs_stat(path .. "/.luarc.json") and not vim.loop.fs_stat(path .. "/.luarc.jsonc") then
-				client.config.settings = vim.tbl_deep_extend("force", client.config.settings.Lua, {
-					runtime = {
-						-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-						version = "LuaJIT",
-						-- Setup your lua path
-						path = runtime_path,
-					},
-					diagnostics = {
-						-- Get the language server to recognize the `vim` global
-						globals = { "vim" },
-					},
-					workspace = {
-						-- Make the server aware of Neovim runtime files
-						library = vim.api.nvim_get_runtime_file("", true),
-						checkThirdParty = false,
-					},
-					-- Do not send telemetry data containing a randomized but unique identifier
-					telemetry = {
-						enable = false,
-					},
-					hint = {
-						enable = true,
-					},
-				})
-
-				client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
-			end
-			return true
-		end,
-	}) ]]
+	local v = vim.version()
+	-- Check if Neovim is at least 0.11.0
+	if v.major > 0 or (v.major == 0 and v.minor >= 11) then
+		vim.lsp.config("lua-language-server", opts)
+	else
+		local lspconfig = require("lspconfig")
+		lspconfig.lua_ls.setup(opts)
+	end
 end
 
 return server
--- nvim/lua/plugin/lsp/server/pyright.lua
+++ nvim/lua/plugin/lsp/server/pyright.lua
@@ -6,15 +6,24 @@ function server.checkOK()
 end
 
 function server.setup()
-	local lspconfig = require("lspconfig")
 	local common = require("plugin.lsp.server.common")
 	local custom_attach = require("core.handlers").on_attach
-	lspconfig.pyright.setup({
-        	on_attach = custom_attach,
+
+	local opts = {
+        on_attach = custom_attach,
 		flags = common.lspflags,
 		capabilities = common.capabilities,
 		filetypes = { "python" },
-	})
+	}
+
+	local v = vim.version()
+	-- Check if Neovim is at least 0.11.0
+	if v.major > 0 or (v.major == 0 and v.minor >= 11) then
+		vim.lsp.config("pyright", opts)
+	else
+		local lspconfig = require("lspconfig")
+		lspconfig.pyright.setup(opts)
+	end
 end
 
 return server
