7cf434d8fb0838e45b1620dbc81624b1c9f61b9e
HangX-Ma <m-contour@qq.com>
2025-11-23 15:41:41 +0800

    feat(lualine): add selected line count
    Enhance the display format for selected characters' line count and
    update the the format like vscode.
    
    Signed-off-by: HangX-Ma <m-contour@qq.com>

--- nvim/lua/plugin/system/lualine.lua
+++ nvim/lua/plugin/system/lualine.lua
@@ -10,35 +10,38 @@ local function virtlocation()
 		local start_line, start_col = start_pos[2], start_pos[3]
 		local end_line, end_col = end_pos[2], end_pos[3]
 
+		-- start < end
 		if start_line > end_line or (start_line == end_line and start_col > end_col) then
 			start_line, end_line = end_line, start_line
 			start_col, end_col = end_col, start_col
 		end
 
 		local count = 0
-		if mode == "\022" then
+		local line_count = end_line - start_line + 1
+
+		if mode == 'V' then
+			-- Visual Line Mode
+			local lines = vim.fn.getline(start_line, end_line)
+			for _, text in ipairs(lines) do
+				count = count + #text
+			end
+			count = count + (line_count - 1) -- LF
+		elseif mode == "\022" then
 			-- Visual Block Mode
 			local lines = vim.fn.getline(start_line, end_line)
 			for _, text in ipairs(lines) do
 				local s_col = math.min(start_col, #text + 1)
 				local e_col = math.min(end_col, #text)
 				if s_col <= e_col then
-					-- count = count + (e_col - s_col + 1) -- LF, Newline break character
+					count = count + (e_col - s_col + 1)
 				end
 			end
-		elseif mode == 'V' then
-			-- Visual Line Mode
-			local lines = vim.fn.getline(start_line, end_line)
-			for _, text in ipairs(lines) do
-				count = count + #text
-			end
-			-- count = count + (#lines - 1) -- LF, Newline break character
+			count = count + (line_count - 1) -- LF
 		else
 			-- Visual Mode
 			if start_line == end_line then
 				local line_text = vim.fn.getline(start_line)
-				local selected_text = string.sub(line_text, start_col, end_col)
-				count = #selected_text
+				count = #string.sub(line_text, start_col, end_col)
 			else
 				local lines = vim.fn.getline(start_line, end_line)
 				for i, text in ipairs(lines) do
@@ -50,15 +53,18 @@ local function virtlocation()
 						count = count + #text
 					end
 				end
-				-- count = count + (#lines - 1) -- LF, Newline break character
+				count = count + (line_count - 1) -- LF
 			end
 		end
 
-		select_info = string.format(" Select(%d)", count)
+		select_info = string.format("%d   %d:%d (%d selected)", line_count, line, vcol, count)
+	else
+		select_info = string.format("%d:%d", line, vcol)
 	end
 
-	return string.format("%d:%d%s", line, vcol, select_info)
+	return select_info
 end
+
 
 -- status bar
 return {
@@ -113,7 +119,14 @@ return {
 				lualine_a = { "mode" },
 				lualine_b = { "branch", "diff", "diagnostics" },
 				lualine_c = { "filename" },
-				lualine_x = { "encoding", "fileformat", "filetype" },
+				lualine_x = {
+					{
+						"encoding",
+						fmt = function(str)
+							return string.upper(str)
+						end
+					}
+					, "fileformat", "filetype" },
 				lualine_y = { "progress" },
 				lualine_z = { virtlocation, { "datetime", style = " %H:%M" } },
 			},
