e751f52cf292b2d6921ee909412713367a78325f
Tofu <m-contour@qq.com>
2026-02-02 14:55:13 +0800

    feat(nvim): Enhance clangd setup with inactive regions support

--- nvim/lua/plugin/lsp/server/clangd.lua
+++ nvim/lua/plugin/lsp/server/clangd.lua
@@ -1,10 +1,25 @@
 local server = {}
 
+require("plugin.lsp.server.inactive_regions").setup()
+
 function server.checkOK()
 	return vim.fn.executable("clangd") == 1
 end
 
 function server.setup()
+	vim.lsp.config('clangd', {
+		capabilities = {
+			offsetEncoding = { "utf-8", "utf-16" },
+			textDocument = {
+				completion = {
+					editsNearCursor = true
+				},
+				inactiveRegionsCapabilities = {
+					inactiveRegions = true,
+				},
+			}
+		}
+	})
 	vim.lsp.enable("clangd")
 end
 
